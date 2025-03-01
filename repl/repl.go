39948b8a2dbe4a87d9351ac77bc6dadd6b596f53
SuperSolik <serpetr99@gmail.com>
2025-03-01 21:10:11 +0100

    repl update

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"supersolik/monkey/lexer"
-	"supersolik/monkey/token"
+	"supersolik/monkey/parser"
 )
 
 const PROMT = "|> "
@@ -15,13 +15,27 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Fprintf(out, PROMT)
-		if scanner.Scan() {
-			line := scanner.Text()
-			l := lexer.New(line)
-
-			for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-				fmt.Fprintf(out, "%+v\n", tok)
-			}
+		if !scanner.Scan() {
+			return
 		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String()+"\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, fmt.Sprintf("\t%s\n", msg))
 	}
 }
